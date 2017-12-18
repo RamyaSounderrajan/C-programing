2ce0eecdb865c22582ae4daeb75bf7a5e46509a3
RamyaSounderrajan <34641764+RamyaSounderrajan@users.noreply.github.com>
2017-12-18 14:58:54 +0530

    Create var.c

---
+++ Beginner/var.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+int i,odi_runs=18426,odi_matches=463;
+float odi_avg=44.83;
+char name[]="Sachin Ramesh Tendulkar",nickname[]="Little Master";
+printf("%s\n",name);
+printf("%s\n",nickname);
+printf("%d\n%.2f\n%d\n",odi_runs,odi_avg,odi_matches);
+}
