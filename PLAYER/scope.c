767e0e47c36220d53d3e04fe77280ab1d0fe53d4
RamyaSounderrajan <34641764+RamyaSounderrajan@users.noreply.github.com>
2017-12-19 11:06:31 +0530

    Create scope.c

---
+++ PLAYER/scope.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int count = 100;          
+
+void main()
+
+{
+
+   int count = 10;         
+
+   printf("Global variable is %d",count);
+
+   printf("Local variable is %d",count);
+  {
+   int count=15;
+   
+   printf("block variable is %d",count);
+  }
+   
+
+}
