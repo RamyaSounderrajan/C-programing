8d417a85762431de47c8c07b460052cc98457120
RamyaSounderrajan <34641764+RamyaSounderrajan@users.noreply.github.com>
2018-01-05 22:02:07 +0530

    Create prime_number.c

---
+++ Problem/prime_number.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int checkPrimeNumber(int n);
+int main()
+{
+    int n1, n2, i, flag;
+      int j, flag = 1;
+    printf("Enter two positive integers: ");
+    scanf("%d %d", &n1, &n2);
+    printf("Prime numbers between %d and %d are: ", n1, n2);
+
+    for(i=n1+1; i<n2; ++i)
+    {
+    for(j=2; j <= n/2; ++j)
+    {
+        if (n%j == 0)
+        {
+            flag =0;
+            break;
+        }
+    }
+        if(flag == 1)
+            printf("%d ",i);
+}
+return 0;
+}
