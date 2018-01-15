01748ea84db95edcda42c77539a28b394b063e49
RamyaSounderrajan <34641764+RamyaSounderrajan@users.noreply.github.com>
2018-01-15 15:29:16 +0530

    Create alphabet_or_not.c

---
+++ Beginner/set1/alphabet_or_not.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+	char a;
+    scanf("%s",&a);	
+    if (((a>='a') && (a<='z')) || ((a>='A') && (a<='Z')))
+    printf("alphabet");
+    else
+    printf("not an alphabet");
+}a
