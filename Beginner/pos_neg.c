82ecb492ed5950b5411dd3de28375377500017a4
RamyaSounderrajan <34641764+RamyaSounderrajan@users.noreply.github.com>
2018-01-15 15:27:04 +0530

    Create pos_neg.c

---
+++ Beginner/pos_neg.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+	int n;
+	scanf("%d",&n);
+	if(n==0)
+	{
+		printf("zero");
+		break;
+	}
+	if(n>0)
+	{
+		printf("posiive");
+	}
+	else
+	{
+		printf("negative");
+	}
+}
