32c03cd79ea6a849edf1be849570bbd499ed0cd5
RamyaSounderrajan <34641764+RamyaSounderrajan@users.noreply.github.com>
2018-01-16 22:25:38 +0530

    Create vowel.c

---
+++ Beginner/set8/vowel.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+	char a[100];
+    scanf("%s",a);	
+    for(i=0;a[i]!=0;i++)
+    {
+
+	    if (a[i]=='a'||a[i]=='e'||a[i]=='i'||a[i]=='o'||a[i]=='u'||a[i]=='A'||a[i]=='E'||a[i]=='I'||a[i]=='O'||a[i]=='U')
+		{
+	
+	    	printf("yes");
+	    	return(0);
+		}
+	}
+	printf("no");
+	return(0);
+}
