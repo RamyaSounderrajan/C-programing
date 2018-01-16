416042f907cf96e358de686ae5a8eef7ffd70889
RamyaSounderrajan <34641764+RamyaSounderrajan@users.noreply.github.com>
2018-01-16 12:21:44 +0530

    Create range_check.c

---
+++ Beginner/set5/range_check.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int n,low,high;
+	scanf("%d",&low);
+	scanf("%d",&high);
+	scanf("%d",&n);
+	if((n>=low) && (n<=high))
+	{
+		printf("number included");
+	}
+	else
+	{
+		printf("not included");
+	}
+}
