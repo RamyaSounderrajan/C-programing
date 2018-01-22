c83580c507614ebddd69b374c4cf9c295a8bd04c
RamyaSounderrajan <34641764+RamyaSounderrajan@users.noreply.github.com>
2018-01-22 18:22:11 +0530

    Update reverse_remove_vowel.c

--- Problem/reverse_remove_vowel.c
+++ Problem/reverse_remove_vowel.c
@@ -8,21 +8,6 @@ int main()
 	scanf("%s",s);
 	n=strlen(s);
 	x=n;
-	if(n==1)
-	{
-		printf("enter more than one character");
-	}
-	for(i=0;i<n;i++)
-	{
-	   if(s[i]<'a' || (s[i]>'z'&&s[i]<'A') || s[i]>'Z') 
-	      printf("number or character encountered\n");
-	      printf("enter only alphabets")
-	      break;
-	}
-	exit(0);
-	
-	if(n>1)
-	{
 	for(i=0;i<n/2;i++)
 	{
 		temp=s[i];
@@ -30,20 +15,22 @@ int main()
 	  	s[x-1]=temp;
 	  	x--;
 	}
-	printf("the reversed string is: %s\n",s);
+	printf("the reversed string");
+	printf("%s\n",s);
 	
-	  for(i=0;i<n;i++)
+	for(i=0;i<n;i++)
 	{
 	if(s[i]=='a'||s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u'||s[i]=='A'||s[i]=='E'||s[i]=='I'||s[i]=='O'||s[i]=='U')
-	{	
-	for(j=i;j<n;j++)
 	{
-	s[j]=s[j+1];
+	j=i;
+	while(j<n)
+	{
+		s[j]=s[j+1];
+	    j++;	
 	}
-	n--;
-	}
-	}
-	printf("final string without vowel:%s",s);
+	n=n-1;
+    }
+    }
+    printf("reversed string aftr removing vowel")
+    printf("%s",s);
 }
-}
-
